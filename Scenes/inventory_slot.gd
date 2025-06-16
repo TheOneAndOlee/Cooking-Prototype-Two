4dc18e678da60fe5d19c4da35faa231b1a54f9d8
Osmond Lee <olee081206@gmail.com>
2025-06-16 14:59:44 -0500

    Added resource files to test

--- Scenes/inventory_slot.gd
+++ Scenes/inventory_slot.gd
@@ -3,3 +3,30 @@ extends Panel
 @onready var count = $ItemCount
 @onready var texture = $ItemTexture
 @onready var outline = self
+
+@export var item: item_data
+
+func _ready():
+	update_slot()
+	var apple = load("res://resources/red_apple.tres")
+	set_item(apple)
+
+func update_slot() -> void:
+	if !item:
+		print("Item not found! Is this a bug?")
+	else:
+		print("Works!")
+		texture = item.texture
+		count = str(item.count)
+		texture.visible = true
+		count.visible = true
+
+
+func set_item(new_item: Resource):
+	self.item = new_item
+	update_slot()
+
+func _on_gui_input(event: InputEvent):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+		if item:
+			print("Clicked on item ", item.name)
---
+++ resources/item_data.gd
@@ -0,0 +1,9 @@
+class_name item_data 
+extends Resource
+
+@export var name: String = ""
+@export var texture: Texture2D
+@export var count: int = 1
+@export var stackable: bool = false
+@export var categories: Dictionary
+@export var traits: Dictionary
